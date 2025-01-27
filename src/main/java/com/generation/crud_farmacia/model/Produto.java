30e458e59dbc5666878aa9201159de9672416fd5
Marcos Iyama <marcosiyama@gmail.com>
2025-01-27 14:37:30 -0300

    Task 3 finalizada

---
+++ src/main/java/com/generation/crud_farmacia/model/Produto.java
@@ -0,0 +1,86 @@
+package com.generation.crud_farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O Atributo Nome é Obrigatório.")
+	@Size(max = 50, message = "O Nome deve ter no máximo 50 caracteres.")
+	private String nome;
+	
+	@NotNull(message = "É necessário infomar o preço.")
+	private String preco;
+	
+	private Integer quantidade;
+	
+	private String datavalidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPreco() {
+		return preco;
+	}
+
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getDatavalidade() {
+		return datavalidade;
+	}
+
+	public void setDatavalidade(String datavalidade) {
+		this.datavalidade = datavalidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+}
